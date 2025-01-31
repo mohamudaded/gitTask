c2e8ced04928c43e65060010911e7f83746bc9e9
aded <aded670101@gmail.com>
2025-01-31 15:21:41 -0800

    added some maths

--- helloWorld.py
+++ helloWorld.py
@@ -5,5 +5,13 @@ color = input("Enter your favorite color:")
 
 if name == "Aded" and color == "blue":
     print(f"Welcome {name}, access granted!")
+    
+    num1 = 5
+    num2 = 7
+    
+    if num1 > num2:
+        num1 * num2
+    else:
+        num2 - num1
 else:
     print("Error, wrong name entered and color. Please try again")
